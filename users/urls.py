ec1f4cb69befdbe661941fa9e20fe3ce7dde0de4
jon1nline <www.jhonattan@hotmail.com>
2025-06-05 14:12:43 -0300

    small code fix in users

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,4 @@ from .views import UsersListCreate, UsersRetrieveUpdateDestroy
 
 urlpatterns = [
     path('users/', UsersListCreate.as_view(), name='users-list-create'),
-   # path('employees/<int:pk>/', UsersRetrieveUpdateDestroy.as_view(), name='employees-retrieve-update-destroy'),
 ]
